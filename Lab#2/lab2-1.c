b0a865b685c92383b933cf6087a1b30fe82bcee0
changi1122 <changi112242@gmail.com>
2020-04-08 13:25:17 +0900

    Lab#2

---
+++ Lab#2/lab2-1.c
@@ -0,0 +1,40 @@
+//EUC-KR
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("----- 이우창  2019038074 -----\n\n");
+
+    char charType;
+    int integerType;
+    float floatType;
+    double doubleType;
+
+    // 각 변수의 크기(바이트) 출력
+    printf("Size of char: %ld byte\n", sizeof(charType));
+    printf("Size of int: %ld byte\n", sizeof(integerType));
+    printf("Size of float: %ld byte\n", sizeof(floatType));
+    printf("Size of double: %ld byte\n", sizeof(doubleType));
+    /* char, int, float, double 변수의 크기를 확인합니다. */
+
+    printf("-----------------------------------------\n");
+
+    // 각 자료형의 크기(바이트) 출력
+    printf("Size of char: %ld byte\n", sizeof(char));
+    printf("Size of int: %ld byte\n", sizeof(int));
+    printf("Size of float: %ld byte\n", sizeof(float));    
+    printf("Size of double: %ld byte\n", sizeof(double));
+    /* char, int, float, double 자료형의 크기를 확인합니다. */
+
+    printf("-----------------------------------------\n");
+
+    // 각 자료형별 포인터의 크기(바이트) 출력
+    printf("Size of char*: %ld byte\n", sizeof(char*));
+    printf("Size of int*: %ld byte\n", sizeof(int*));
+    printf("Size of float*: %ld byte\n", sizeof(float*));
+    printf("Size of double*: %ld byte\n", sizeof(double*));
+    /* 포인터 변수는 변수의 주소를 담기에 자료형이 달라도 그 크기는 같다는 걸 알 수 있습니다. */
+
+    return 0;
+}
---
+++ Lab#2/lab2-2.c
@@ -0,0 +1,70 @@
+//EUC-KR
+
+#include <stdio.h>
+
+int main()
+{
+    printf("----- 이우창  2019038074 -----\n\n");
+
+    int i;
+    int *ptr;
+    int **dptr;
+
+    i = 1234;
+    
+    printf("[checking values before ptr = &i]\n");
+    printf("value of i == %d\n", i);
+    printf("address of i == %p\n", &i);
+    printf("value of ptr == %p\n", ptr);
+    printf("address of ptr == %p\n", &ptr);
+
+    // ptr에 i의 주소 대입
+    ptr = &i;
+
+    printf("\n[checking values after ptr = &i]\n");
+    printf("value of i == %d\n", i);
+    printf("address of i == %p\n", &i);
+    printf("value of ptr == %p\n", ptr);
+    printf("address of ptr == %p\n", &ptr);
+    printf("value of *ptr == %d\n", *ptr);
+
+    /* 포인터 ptr의 담고 있는 주소, 자신의 주소, 저장된 i의 주소를 이용해 역참조하는 걸 보입니다. */
+
+    // dptr에 ptr의 주소 대입
+    dptr = &ptr;
+
+    printf("\n[checking values after dptr = &ptr]\n");
+    printf("value of i == %d\n", i);
+    printf("address of i == %p\n", &i);
+    printf("value of ptr == %p\n", ptr);
+    printf("address of ptr == %p\n", &ptr);
+    printf("value of *ptr == %d\n", *ptr);
+    printf("value of dptr == %p\n", dptr);
+    printf("address of dptr == %p\n", &dptr);
+    printf("value of *dptr == %p\n", *dptr);
+    printf("value of **dptr == %d\n", **dptr);
+
+    /* 이중 포인터 dptr에 대해 담고 있는 주소, 자신의 주소, 한번 역참조한 값, 두번 역참조한 값을 보입니다. */
+
+    // *ptr의 값 변경
+    *ptr = 7777;
+    
+    printf("\n[after *ptr = 7777]\n");
+    printf("value of i == %d\n", i);
+    printf("value of *ptr == %d\n", *ptr);
+    printf("value of **dptr == %d\n", **dptr);
+    
+    /* ptr의 값을 이용해 i를 변경한 걸 보입니다. */
+
+    // **dptr의 값 변경
+    **dptr = 8888;
+
+    printf("\n[after **dptr = 8888]\n");
+    printf("value of i == %d\n", i);
+    printf("value of *ptr == %d\n", *ptr);
+    printf("value of **dptr == %d\n", **dptr);
+
+    /* 이중 포인터 dptr의 값을 이용해 i를 변경한 걸 보입니다. */
+
+    return 0;
+}
